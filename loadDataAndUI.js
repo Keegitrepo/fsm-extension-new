d0001fc2880e7aa451a2e401738a0e692f36903c
Keerthana L <keerthana.l@evorait.com>
2024-03-11 17:29:15 +0530

    token refresh timeout

--- loadDataAndUI.js
+++ loadDataAndUI.js
@@ -3,16 +3,16 @@ const emergencyList = document.getElementById('emergencyList');
 const sameDayList = document.getElementById('sameDayList');
 
 // // Function to add items to the list
-function addItemToList(listId, link, text, associatedObject={}) {
+function addItemToList(listId, link, text, associatedObject = {}) {
     const list = document.getElementById(listId);
     const listItem = document.createElement('li');
     const anchor = document.createElement('a');
-    
+
     // Set href, text content, and target for the anchor
     anchor.href = link;
     anchor.textContent = text;
     anchor.target = "_blank";
-    
+
     // Set the associated object as a data attribute
     anchor.dataset.associatedObject = JSON.stringify(associatedObject);
 
@@ -20,34 +20,20 @@ function addItemToList(listId, link, text, associatedObject={}) {
     list.appendChild(listItem);
 }
 
-// Example usage:
-// const myObject1 = { id: 1, name: 'Object 1' };
-// const myObject2 = { id: 2, name: 'Object 2' };
-
-// addItemToList('emergencyList', 'https://example.com', 'Item 1', myObject1);
-// addItemToList('sameDayList', 'https://example.com', 'Item 2', myObject2);
-
-
-function createMapUrlAndAddItemToList(listId, responseData, cloudHost) {
+function createMapUrlAndAddItemToList(listId, responseData) {
     if (responseData && responseData.data && responseData.data.length) {
         let paramsArray = responseData.data.forEach((dataObj) => {
-            try {
-                let {act, scall, add} = dataObj;
-                // URL to make - https://us.coresystems.net/shell/#/planning-dispatching/map/date/latitude,longitude/activities/activityId
-                let {latitude, longitude} = add.location ? add.location : {latitude: 0, longitude: 0}; // if there's no lat and long data, we're defauilting to 0 & 0 so as to avoid the failure case
-                let {id: activityId, code} = act;
-                let mapDate = new Date().toISOString().substring(0, 10);
-                let mapUrlForCurrentActivity = `https://us.coresystems.net/shell/#/planning-dispatching/map/${mapDate}/${latitude},${longitude},z11/activities/${activityId}`;
-                let itemText = `${scall.code} - Map`;
-                addItemToList(listId, mapUrlForCurrentActivity, itemText);
-            } catch (error) {
-                let err;
-            }
+            let { act, scall, add } = dataObj;
+            // URL to make - https://us.coresystems.net/shell/#/planning-dispatching/map/date/latitude,longitude/activities/activityId
+            let { latitude, longitude } = add?.location ? add.location : { latitude: 0, longitude: 0 }; // if there's no lat and long data, we're defauilting to 0 & 0 so as to avoid the failure case
+            let { id: activityId, code } = act;
+            let mapDate = new Date().toISOString().substring(0, 10);
+            let mapUrlForCurrentActivity = `https://us.coresystems.net/shell/#/planning-dispatching/map/${mapDate}/${latitude},${longitude},z11/activities/${activityId}`;
+            let itemText = `${scall.code} - Map`;
+            addItemToList(listId, mapUrlForCurrentActivity, itemText);
         })
     }
 }
-
-
 
 document.getElementById('timeLink').addEventListener('click', onTimeClick);
 let isTimerUpdated = false;
@@ -58,48 +44,48 @@ function onTimeClick() {
         isTimerUpdated = true;
         const controlsContainer = document.createElement('div');
         controlsContainer.classList.add('controls-container');
-        controlsContainer.setAttribute('id','timerId');
-    
+        controlsContainer.setAttribute('id', 'timerId');
+
         // Create a new input control
         const inputControl = document.createElement('input');
-        inputControl.setAttribute('id','inputId');
+        inputControl.setAttribute('id', 'inputId');
         inputControl.placeholder = 'Enter time...';
         inputControl.type = 'text'; // Use 'text' to allow pattern validation
         inputControl.value = 10;
         previousInputValue = 10;
-    
+
         // Validate and sanitize input on each change
         inputControl.addEventListener('input', function () {
             this.value = this.value.replace(/[^0-9]/g, ''); // Allow only numeric input
         });
-    
+
         // Create a div for vertical alignment
         const inputButtonsContainer = document.createElement('div');
-    
+
         // Create two buttons (check and cross)
         const checkButton = document.createElement('button');
         checkButton.innerHTML = 'Save';
         checkButton.classList.add('button-save'); // Add a class for styling
         checkButton.addEventListener('click', onTimerSave);
-    
+
         const crossButton = document.createElement('button');
         crossButton.innerHTML = 'Cancel';
         crossButton.classList.add('button-cancel'); // Add a class for styling
         crossButton.addEventListener('click', onTimerCancel);
-    
+
         // Append the input control to the container
         controlsContainer.appendChild(inputControl);
-    
+
         // Append buttons to the buttons container
         inputButtonsContainer.appendChild(checkButton);
         inputButtonsContainer.appendChild(crossButton);
-    
+
         // Append the buttons container to the main container
         controlsContainer.appendChild(inputButtonsContainer);
-    
+
         // Append the container to the body
         document.body.appendChild(controlsContainer);
-    
+
         // Disable other controls
         disableControls();
     } else {
@@ -113,13 +99,12 @@ async function onTimerSave() {
     document.getElementById("emergencyList").innerHTML = '';
     document.getElementById("sameDayList").innerHTML = '';
 
-    clearTimeout(shellReferenceObject['emergencyList']);
-    clearTimeout(shellReferenceObject['sameDayList']);
-    
     enableControls();
-    // Here we've to fetch the data with the previousInputValue as the timer
-    await fetchData('emergencyList', globalCompanyObject, { "query": "select rr.id,rr.code, act.id,act.udf.ZZEMRALERT , act.externalId, act.startDateTime, act.code, act.timeZoneId, scall.code, scall.subject, scall.createDateTime, add.location, eq.id as equipment_id from ServiceCall scall INNER JOIN Activity act ON act.object.objectId = scall.id INNER JOIN Address add ON add.id = act.address INNER JOIN Region rr ON rr.id = act.region INNER JOIN Equipment eq ON eq.id = act.equipment WHERE scall.priority = 'HIGH' AND scall.typeCode = 'GEMR' AND act.status = 'DRAFT' AND act.executionStage = 'DISPATCHING'"});
-    await fetchData('sameDayList', globalCompanyObject, { "query": "select act.id, act.createDateTime, act.code, scall.code, scall.subject, add.location, add.location from ServiceCall scall INNER JOIN Activity act ON act.object.objectId = scall.id INNER JOIN Address add ON add.id = act.address WHERE scall.priority = 'HIGH' AND scall.typeCode != 'GEMR' AND act.status = 'DRAFT' AND act.executionStage = 'DISPATCHING'"});
+    let [shellSdk, SHELL_EVENTS] = [shellReferenceObject["shellSdk"], shellReferenceObject["SHELL_EVENTS"]];
+    clearTimeout(globalTimeOutId);
+    shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
+        response_type: 'token'
+    });
 }
 
 function onTimerCancel() {
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 var globalCompanyObject;
 var shellReferenceObject = {};
+let globalTimeOutId;
+
 function updateWarning(text) {
     document.getElementById("greet").innerHTML = text
 }
@@ -33,78 +35,53 @@ function isInsideShell(FSMShell) {
             // Each extension need to implement its own strategy to fresh it.
             shellReferenceObject["shellSdk"] = shellSdk;
             shellReferenceObject["SHELL_EVENTS"] = SHELL_EVENTS;
-            shellReferenceObject["auth"] = auth;
             shellReferenceObject["jsonEvent"] = JSON.parse(event);
-            initializeRefreshTokenStrategy(shellSdk, SHELL_EVENTS, auth, JSON.parse(event));
+
+            return callbackLimiter(shellSdk, SHELL_EVENTS, globalCompanyObject);
         });
     }
 }
-async function postUpdatedZZEMRALERTValue(comapnyObject, patchRequestBody, act) {
-    try {
-        const { cloudHost, accountId, companyId } = comapnyObject;
-        const header = {
-            "Content-Type": "application/json",
-            "X-Client-ID": "000179c6-c140-44ec-b48e-b447949fd5c9",
-            "X-Client-Version": "1.0",
-            "Authorization": `bearer ${sessionStorage.getItem('token')}`,
-            "X-Account-ID": accountId,
-            "X-Company-ID": companyId
-        };
-        let url = `https://${cloudHost}/api/data/v4/Activity/externalId/${act.externalId}?dtos=Activity.43&forceUpdate=true`;
-        let body = JSON.stringify(patchRequestBody);
-        let method = 'PATCH';
-        // Make the POST request to update the ZZEMRALERT value
-        const response = await fetch(url, {
-            method: method,
-            headers: header,
-            body: body
-        });
 
-        // Check if the request was successful
-        if (!response.ok) {
-            throw new Error('Failed to update ZZEMRALERT value');
-        }
-
-        // Handle the response if needed
-        const responseData = await response.json();
-        console.log('Updated ZZEMRALERT value:', responseData);
-        return true;
-    } catch (error) {
-        console.error('Error updating ZZEMRALERT value:', error.message);
-        // Handle errors appropriately, such as displaying an error message
-    }
-}
-// Loop before a token expire to fetch a new one
-async function initializeRefreshTokenStrategy(shellSdk, SHELL_EVENTS, auth, comapnyObject) {
-    shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
-        sessionStorage.setItem('token', event.access_token);
-        setTimeout(() => fetchToken(), (event.expires_in * 1000) - 10000);
+async function callbackLimiter(shellSdk, SHELL_EVENTS, globalCompanyObject) {
+    shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
+        response_type: 'token'
     });
-
-    function fetchToken() {
-        shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
-            response_type: 'token'  // request a user token within the context
-        });
-    }
-
-    sessionStorage.setItem('token', auth.access_token);
-    setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 10000);
-
-    await fetchData('emergencyList', comapnyObject, { "query": "select rr.id,rr.code, act.id, act.externalId, act.udf.ZZEMRALERT , act.startDateTime, act.code, act.timeZoneId, scall.code, scall.subject, scall.createDateTime, add.location, eq.id as equipment_id from ServiceCall scall INNER JOIN Activity act ON act.object.objectId = scall.id INNER JOIN Address add ON add.id = act.address INNER JOIN Region rr ON rr.id = act.region INNER JOIN Equipment eq ON eq.id = act.equipment WHERE scall.priority = 'HIGH' AND scall.typeCode = 'GEMR' AND act.status = 'DRAFT' AND act.executionStage = 'DISPATCHING'"}); // For Emergency orders
-    await fetchData('sameDayList', comapnyObject, { "query": "select act.id, act.createDateTime, act.code, scall.code, scall.subject, add.location, add.location from ServiceCall scall INNER JOIN Activity act ON act.object.objectId = scall.id INNER JOIN Address add ON add.id = act.address WHERE scall.priority = 'HIGH' AND scall.typeCode != 'GEMR' AND act.status = 'DRAFT' AND act.executionStage = 'DISPATCHING'"}); // For Same day orders
+    return refreshTokenNFetchData(shellSdk, SHELL_EVENTS, globalCompanyObject);
 }
-let previousEmergencyCount = 0;
-async function fetchData(listId, comapnyObject, queryObj) {
-    // Next call for loading the data asynchronously time to time
-    let inputValue = document.getElementById("inputId") ? document.getElementById("inputId").value : 10; // i.e default value
-    let loadDataTimePeriod = Number(inputValue) * 60 * 1000; // time in milli seconds i.e 1min * 60sec * 1000ms
-    let id = setTimeout((listId, comapnyObject) => {
-        fetchData(listId, comapnyObject);
-    }, loadDataTimePeriod, listId, comapnyObject);
-    shellReferenceObject[`${listId}`] = id;
+
+async function refreshTokenNFetchData(shellSdk, SHELL_EVENTS, globalCompanyObject) {
+    clearTimeout(globalTimeOutId); // Here we clear the previous timeout id that's stored
+
+    // Response for the request
+    shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, async (event) => {
+        clearTimeout(globalTimeOutId);
+        sessionStorage.setItem('token', event.access_token);
+
+        // Next call for loading the data asynchronously time to time
+        let inputValue = document.getElementById("inputId") ? document.getElementById("inputId").value : 10; // i.e default value
+        let loadDataTimePeriod = Number(inputValue) * 60 * 1000; // time in milli seconds i.e 1min * 60sec * 1000ms
+        globalTimeOutId = setTimeout((shellSdk, SHELL_EVENTS) => {
+            shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
+                response_type: 'token'
+            });
+        }, loadDataTimePeriod, shellSdk, SHELL_EVENTS);
+
+        return fetchData(globalCompanyObject);
+    });
+}
+
+async function fetchData(comapnyObject) {
+    const sameDayObj = {
+        'listName': 'sameDayList',
+        'body': JSON.stringify({ "query": "select act.id, act.createDateTime, act.code, scall.code, scall.subject, add.location, add.location from ServiceCall scall INNER JOIN Activity act ON act.object.objectId = scall.id INNER JOIN Address add ON add.id = act.address WHERE scall.priority = 'HIGH' AND scall.typeCode != 'GEMR' AND act.status = 'DRAFT' AND act.executionStage = 'DISPATCHING'" })
+    };
+
+    const emergencyObj = {
+        'listName': 'emergencyList',
+        'body': JSON.stringify({ "query": "select rr.id,rr.code, act.id,act.udf.ZZEMRALERT , act.externalId , act.startDateTime, act.code, act.timeZoneId, scall.code, scall.subject, scall.createDateTime, add.location, eq.id as equipment_id from ServiceCall scall INNER JOIN Activity act ON act.object.objectId = scall.id INNER JOIN Address add ON add.id = act.address INNER JOIN Region rr ON rr.id = act.region INNER JOIN Equipment eq ON eq.id = act.equipment WHERE scall.priority = 'HIGH' AND scall.typeCode = 'GEMR' AND act.status = 'DRAFT' AND act.executionStage = 'DISPATCHING'" })
+    };
 
     const { cloudHost, account, company, accountId, companyId } = comapnyObject; // extract required context from event content
-
     const header = {
         "Content-Type": "application/json",
         "X-Client-ID": "000179c6-c140-44ec-b48e-b447949fd5c9",
@@ -113,29 +90,26 @@ async function fetchData(listId, comapnyObject, queryObj) {
         "X-Account-ID": accountId,
         "X-Company-ID": companyId
     };
-    let url = `https://${cloudHost}/api/query/v1?account=${account}&company=${company}&dtos=Activity.43;ServiceCall.27;Address.22;Region.9;Equipment.24`
-    let body = JSON.stringify(queryObj);
+    let url = `https://${cloudHost}/api/query/v1?account=${account}&company=${company}&dtos=Activity.43;ServiceCall.27;Address.22;Region.9;Equipment.24`;
     let method = 'POST';
 
-    try {
-        let response = await fetch(url, {
-            method: method,
-            headers: header,
-            body: body
-        });
-        if (!response.ok) {throw false};
+    return Promise.all([fetch(url, { method: method, headers: header, body: sameDayObj['body'] }), fetch(url, { method: method, headers: header, body: emergencyObj['body'] })]).then(responses => {
+        return Promise.all(responses.map(response => response.json()))
+    }).then(data => {
+        const [data1, data2] = data;
+        document.getElementById('sameDayList').innerHTML = '';
+        createMapUrlAndAddItemToList('sameDayList', data1);
 
-        let jsonResponse = await response.json();
-        document.getElementById(listId).innerHTML = '';
-        createMapUrlAndAddItemToList(listId, jsonResponse, cloudHost);
-        if (listId === 'emergencyList' && jsonResponse.data && jsonResponse.data.length > 0){
-            jsonResponse.data.forEach(async data => {
-                let {scall, rr, act, equipment_id } = data;
+        document.getElementById('emergencyList').innerHTML = '';
+        createMapUrlAndAddItemToList('emergencyList', data2);
+
+        if (data2.data && data2.data.length > 0) {
+            data2.data.forEach(async data => {
+                let { scall, rr, act, equipment_id } = data;
                 let premise = equipment_id ? "Dispatcher Area" : "Off-Premise";
-                if (act && Array.isArray(act.udfValues)){
-                let ZZEMRALERT = act.udfValues.find(udf => udf.name === "ZZEMRALERT");
+                if (act && Array.isArray(act.udfValues)) {
+                    let ZZEMRALERT = act.udfValues.find(udf => udf.name === "ZZEMRALERT");
                     if (ZZEMRALERT && ZZEMRALERT.value === "false") {
-                        alert(`New Emergency Received Service Order #${scall.code}, Work Center: ${rr.code.substring(8)}, Premise: ${premise}`);
                         //Construct the PATCH request body
                         let patchRequestBody = {
                             "udfValues": [
@@ -147,28 +121,33 @@ async function fetchData(listId, comapnyObject, queryObj) {
                                 }
                             ]
                         };
-                        await postUpdatedZZEMRALERTValue(comapnyObject, patchRequestBody, act);
-                   }
+                        const { cloudHost, accountId, companyId } = comapnyObject;
+                        const header = {
+                            "Content-Type": "application/json",
+                            "X-Client-ID": "000179c6-c140-44ec-b48e-b447949fd5c9",
+                            "X-Client-Version": "1.0",
+                            "Authorization": `bearer ${sessionStorage.getItem('token')}`,
+                            "X-Account-ID": accountId,
+                            "X-Company-ID": companyId
+                        };
+                        let url = `https://${cloudHost}/api/data/v4/Activity/externalId/${act.externalId}?dtos=Activity.43&forceUpdate=true`;
+                        let body = JSON.stringify(patchRequestBody);
+                        let method = 'PATCH';
+
+                        return Promise.resolve(fetch(url, {
+                            method: method,
+                            headers: header,
+                            body: body
+                        })).then(response => {
+                            return Promise.resolve(response.json());
+                        }).then(data => {
+                            alert(`New Emergency Received Service Order #${scall.code}, Work Center: ${rr.code.substring(8)}, Premise: ${premise}`);
+                        })
+                    }
                 }
             });
         }
-        return true
-    } catch (error) {
-        document.getElementById('emergencyList').innerHTML = '';
-        document.getElementById('sameDayList').innerHTML = '';
-        
-        clearTimeout(shellReferenceObject['emergencyList']);
-        clearTimeout(shellReferenceObject['sameDayList']);
-
-        shellReferenceObject.shellSdk.emit(shellReferenceObject["SHELL_EVENTS"].Version1.REQUIRE_AUTHENTICATION, {
-            response_type: 'token'  // request a user token within the context
-        });
-
-        shellReferenceObject.shellSdk.on(shellReferenceObject["SHELL_EVENTS"].Version1.REQUIRE_AUTHENTICATION, async (event) => {
-            sessionStorage.setItem('token', event.access_token);
-            await fetchData('emergencyList', comapnyObject, { "query": "select rr.id,rr.code, act.id,act.udf.ZZEMRALERT , act.externalId , act.startDateTime, act.code, act.timeZoneId, scall.code, scall.subject, scall.createDateTime, add.location, eq.id as equipment_id from ServiceCall scall INNER JOIN Activity act ON act.object.objectId = scall.id INNER JOIN Address add ON add.id = act.address INNER JOIN Region rr ON rr.id = act.region INNER JOIN Equipment eq ON eq.id = act.equipment WHERE scall.priority = 'HIGH' AND scall.typeCode = 'GEMR' AND act.status = 'DRAFT' AND act.executionStage = 'DISPATCHING'"}); // For Emergency orders
-            await fetchData('sameDayList', comapnyObject, { "query": "select act.id, act.createDateTime, act.code, scall.code, scall.subject, add.location, add.location from ServiceCall scall INNER JOIN Activity act ON act.object.objectId = scall.id INNER JOIN Address add ON add.id = act.address WHERE scall.priority = 'HIGH' AND scall.typeCode != 'GEMR' AND act.status = 'DRAFT' AND act.executionStage = 'DISPATCHING'"}); // For Same day orders
-        });
-
-    }
+    }).catch(error => {
+        alert(`Some thing went wrong with the extension, Please reload the page manually`);
+    })
 }
